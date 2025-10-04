17f83e8fed1e5612eec33db269f2bc901ebd2f6d
valdigley <valdigley2007@gmail.com>
2025-10-04 01:04:39 -0300

    Updated index.css

--- src/components/Toast/ToastContainer.tsx
+++ src/components/Toast/ToastContainer.tsx
@@ -8,7 +8,7 @@ interface ToastContainerProps {
 
 export function ToastContainer({ toasts, onClose }: ToastContainerProps) {
   return (
-    <div className="fixed top-4 right-4 z-50 flex flex-col items-end">
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 flex flex-col items-center">
       {toasts.map((toast) => (
         <Toast key={toast.id} {...toast} onClose={onClose} />
       ))}
